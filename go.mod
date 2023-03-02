f9260b3938255bd5ebe56bbf89b769ba3ef63039
markyjackson-taulia <marky.r.jackson@gmail.com>
2023-03-01 20:26:19 -0800

    This is a Go program that prompts the user to enter a baseball team's abbreviation, then uses that abbreviation to construct a URL and make an HTTP request to a website that provides baseball statistics. Specifically, it retrieves the batting statistics for the team's players during the 2022 season. The program then parses the HTML response to extract the data from the table and creates a matrix from it using the gonum library.
    The program then computes the regression coefficients and uses them to predict the team's wins for the upcoming season. It prints the predicted number of wins to the console.

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/markyjackson-taulia/sf_giants_stats
 
 go 1.20
 
-require github.com/PuerkitoBio/goquery v1.8.1
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	gonum.org/v1/gonum v0.12.0
+)
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
@@ -47,24 +49,81 @@ func run(input io.Reader, output io.Writer) {
 	}
 
 	headers := make([]string, 0)
-	rows := make([][]string, 0)
-	table.Find("thead tr th, tbody tr:not(.thead) td").Each(func(i int, header *goquery.Selection) {
-		headers = append(headers, strings.TrimSpace(header.Text()))
-	})
-	table.Find("tbody tr:not(.thead)").Each(func(i int, row *goquery.Selection) {
+	var data [][]float64
+	table.Find("thead tr th, tbody tr:not(.thead)").Each(func(i int, row *goquery.Selection) {
 		cells := make([]string, 0)
-		row.Find("td").Each(func(j int, cell *goquery.Selection) {
+		row.Find("th, td").Each(func(j int, cell *goquery.Selection) {
 			cells = append(cells, strings.TrimSpace(cell.Text()))
 		})
-		rows = append(rows, cells)
+		if len(cells) > 0 {
+			if i == 0 {
+				headers = cells[1:]
+			} else {
+				vals := make([]float64, len(cells)-1)
+				for k, v := range cells[1:] {
+					if f, err := strconv.ParseFloat(v, 64); err == nil {
+						vals[k] = f
+					} else {
+						// Skip non-numeric values
+						continue
+					}
+				}
+				data = append(data, vals)
+			}
+		}
 	})
 
-	// Print the headers and the team's stats for the 2022 season
-	fmt.Fprintln(output, strings.Join(headers, "\t"))
-	for _, row := range rows {
-		if len(row) > 0 && row[0] == "2022" {
-			fmt.Fprintln(output, strings.Join(row, "\t"))
-			break
-		}
+	// Check that the data slice is not empty
+	if len(data) == 0 {
+		fmt.Fprintln(output, "No data found in HTML")
+		return
 	}
+
+	// Create a dense matrix from the data slice
+	X := mat.NewDense(len(data), len(headers)-1, nil)
+	Y := mat.NewVecDense(len(data), nil)
+	for i, row := range data {
+		if len(row) != len(headers) {
+			log.Fatalf("Error parsing data: row %d has length %d instead of %d", i, len(row), len(headers))
+		}
+		for j, val := range row[1:] {
+			X.Set(i, j, val)
+		}
+		Y.SetVec(i, row[0])
+	}
+
+	// Compute the means of X and Y
+	meanX := mat.Sum(X.ColView(0)) / float64(X.RawMatrix().Rows)
+	meanY := mat.Sum(Y) / float64(Y.Len())
+
+	// Compute the variances and covariance of X and Y
+	varX := 0.0
+	varY := 0.0
+	covXY := 0.0
+	for i := 0; i < X.RawMatrix().Rows; i++ {
+		x := X.At(i, 0)
+		y := Y.AtVec(i)
+		devX := x - meanX
+		devY := y - meanY
+		varX += devX * devX
+		varY += devY * devY
+		covXY += devX * devY
+	}
+	varX /= float64(X.RawMatrix().Rows)
+	varY /= float64(Y.Len())
+	covXY /= float64(X.RawMatrix().Rows)
+
+	// Compute the regression coefficients
+	beta := covXY / varX
+	alpha := meanY - beta*meanX
+
+	// Use the regression coefficients to predict the team's wins
+	wins := 0.0
+	for i := 0; i < len(headers)-1; i++ {
+		wins += beta * X.At(0, i)
+	}
+	wins += alpha
+
+	// Print the predicted wins for the upcoming season
+	fmt.Fprintf(output, "\nPredicted wins for the upcoming season: %.2f\n", wins)
 }
